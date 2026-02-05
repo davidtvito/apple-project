bed2bae88c498fd20be6b32fa67fd352aad79b04
David Tvito <davidtvito05832@gmail.com>
2026-02-05 11:17:31 +0200

    with REST

---
+++ src/main/java/com/example/apple_project/controller/IphoneController.java
@@ -0,0 +1,51 @@
+package com.example.apple_project.controller;
+
+import com.example.apple_project.dto.IphoneIn;
+import com.example.apple_project.model.Iphone;
+import com.example.apple_project.service.IphoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/iphone")
+public class IphoneController {
+
+    @Autowired
+    IphoneService iphoneService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllIphones() {
+        return new ResponseEntity<>(iphoneService.all(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneIphone(@PathVariable Long id) {
+        return new ResponseEntity<>(iphoneService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insertIphone(@RequestBody IphoneIn iphoneIn) {
+        Iphone iphone = iphoneIn.toIphone(iphoneIn);
+        iphone = iphoneService.save(iphone);
+        return new ResponseEntity<>(iphone, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateIphone(@PathVariable Long id, @RequestBody IphoneIn iphoneIn) {
+        Optional<Iphone> dbIphone = iphoneService.findById(id);
+        iphoneIn.updateIphone(dbIphone.get());
+        Iphone updatedIphone = iphoneService.save(dbIphone.get());
+        return new ResponseEntity<>(updatedIphone, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteIphone(@PathVariable Long id) {
+        Optional<Iphone> dbIphone = iphoneService.findById(id);
+        iphoneService.delete(dbIphone.get());
+        return new ResponseEntity<>("DELETED", HttpStatus.OK);
+    }
+}
